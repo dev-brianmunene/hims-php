cc444088e4d4ed865532aac4d9d21c5f8c0714d8
dev-brianmunene <munenebrian951@gmail.com>
2025-07-26 16:28:44 +0300

    hims first commit

--- backend/doc/assets/inc/footer.php
+++ backend/doc/assets/inc/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                2020 - <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By Brian Munene</a>
+                2022 - <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By Brian Munene</a>
             </div>
 
         </div>
